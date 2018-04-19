5a6e8371ee0568a6cc4292e6cf39af60f6244876
pastapojken <pastapojken@users.noreply.github.com>
2018-04-19 12:21:05 +0200

    Update jLoginaze.sh

--- jLoginaze.sh
+++ jLoginaze.sh
@@ -12,5 +12,5 @@ cd "$(dirname "$0")"
 #file=$(basename "$1")
 file="${1%.*}"
 
-sed -i "1s/^/<?php\nsession_start();\nif (isset(\$_SESSION['login_user']) == false || empty($_SESSION['login_user'])) {\nheader('Location:login.html');\n}\n?>\n/" $1
+sed -i "1s/^/<?php\nsession_start();\nif (isset(\$_SESSION['login_user']) == false || empty(\$_SESSION['login_user'])) {\nheader('Location:login.html');\n}\n?>\n/" $1
 mv $1 $file".php"
